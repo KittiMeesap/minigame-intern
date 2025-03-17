24260e44d59515ec702f1be71f15155deadf675e
kitti <tkt.ms@hotmail.com>
2025-03-17 22:06:25 +0700

    Checkpoint Test

--- Minigame/Assets/Scripts/CheckPointFunction.cs
+++ Minigame/Assets/Scripts/CheckPointFunction.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class CheckPointFunction : MonoBehaviour
 {
+    public GameObject player;
+    private bool isChecked;
+    private Vector3 checkedPosition;
+    private float checkedTime;
+    public Renderer cpRenderer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        checkedPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isChecked)
+        {
+            cpRenderer.material.color = Color.black;
+        }
     }
 }
