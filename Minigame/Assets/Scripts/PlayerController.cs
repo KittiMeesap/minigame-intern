ed7cb8418a2f5d5cde1a1ed501a450d03d678b99
kitti <tkt.ms@hotmail.com>
2025-03-17 18:53:45 +0700

    Add PlayerMove Script

--- Minigame/Assets/Scripts/PlayerController.cs
+++ Minigame/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    private Vector3 playerMove;
+    public float playerSpeed = 5f;
+
+    private bool isMoving;
+    private bool isHitWall;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerMove.x = Input.GetAxis("Horizontal");
+        playerMove.z = Input.GetAxis("Vertical");
     }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = playerMove.normalized * playerSpeed;
+    }
+
 }
