7bf19a022ce9ae60bcf6abe19df7d31e904ef31c
kitti <tkt.ms@hotmail.com>
2025-03-17 20:00:09 +0700

    Update Move

--- Minigame/Assets/Scripts/PlayerController.cs
+++ Minigame/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    private Vector3 playerMove;
-    public float playerSpeed = 5f;
+    private Vector3 playerDirection = Vector3.zero;
+    public float playerSpeed = 10f;
 
-    private bool isMoving;
-    private bool isHitWall;
+    private bool isMoving = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,21 +20,55 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerMove.x = Input.GetAxis("Horizontal");
-        playerMove.z = Input.GetAxis("Vertical");
+        PlayerInput();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.linearVelocity = playerMove.normalized * playerSpeed;
+        if (isMoving)
+        {
+            rb.linearVelocity = playerDirection * playerSpeed;
+        }
     }
 
     private void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.name == "Wall" || col.gameObject.CompareTag("Wall"))
+        if (col.gameObject.name == "Wall" || col.gameObject.CompareTag("Wall"))
         {
             rb.linearVelocity = Vector3.zero;
+            isMoving = false;
         }
+    }
+
+    private void PlayerInput()
+    {
+        if (!isMoving)
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                playerDirection = Vector3.forward;
+                isMoving = true;
+            }
+
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                playerDirection = Vector3.down;
+                isMoving = true;
+            }
+
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                playerDirection = Vector3.left;
+                isMoving = true;
+            }
+
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                playerDirection = Vector3.right;
+                isMoving = true;
+            }
+        }
+
     }
 
 }
